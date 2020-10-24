2767c33d246faf49bf6aeb01cb469e3ad337726d
Narendran <narendev0610@gmail.com>
2020-10-24 14:15:57 +0530

    folder structure done, readme added

---
+++ hcl/README.md
@@ -0,0 +1,9 @@
+## terraform folder structure
+
+So, for starters the terraform folder consists of 
+* main.tf 
+* variables.tf 
+* outputs.tf
+
+If the `main.tf` file gets bigger than we might use additional `.tf` files, but that's the good time to start using 
+terraform modules.
--- hcl/main.tf
+++ hcl/main.tf
@@ -1,9 +1,30 @@
+#Terraform
+# Terraform current cli version is `v0.13.5`
+
 terraform {
   required_version = ">= 0.13.0"
 }
+
+#Provider
+# Implement cloud specific API and Terraform API.
+# Provider configuation is specific to each provider.
+# Providers expose Data Sources and Resources to Terraform.
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-east-1"
+  #access_key = "my-access-key"
+  #secret_key = "my-secret-key"
+
+  #Many providers also accept configuration via environment variables
+  #or config files.  The AWS provider will read the standard AWS CLI
+  #settings if they are present.
+}
+
+
 
 output "greeting" {
   value = "Hello Terraform."
 }
 
-provider "random"{}
+# this was a random provider.
+# provider "random" {}
