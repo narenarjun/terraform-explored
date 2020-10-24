d07186525ac03db6205c79503b2df2622f7c1e74
Narendran <narendev0610@gmail.com>
2020-10-24 15:49:22 +0530

    resources created, custom name for aws bucket done

--- hcl/main.tf
+++ hcl/main.tf
@@ -20,7 +20,24 @@ provider "aws" {
   #settings if they are present.
 }
 
+#Resources
+# Objects managed by Terraform such as VMs or S3 Buckets.
+# Declaring a Resource tells Terraform that it should CREATE
+# and manage the Resource described.  If the Resource already exists
+# it must be imported into Terraform's state.
+resource "aws_s3_bucket" "bucket1" {
+    bucket = "backet1r"
+}
 
+
+#Data Sources
+# Objects NOT managed by Terraform.{terraform is not responsible for the existences of these objects}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
 
 output "greeting" {
   value = "Hello Terraform."
